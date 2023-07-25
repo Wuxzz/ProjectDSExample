91ed436d8d5e79f8e5f072ea04d212abb1f12749
Wuxzz <kevin.dariuz@gmail.com>
2023-07-25 14:42:01 -0600

    update

--- src/generate_histogram.py
+++ src/generate_histogram.py
@@ -1,7 +1,7 @@
 # src/generate_figures.py
 import pandas as pd
 import matplotlib.pyplot as plt
-
+import helpers.DataLoader as Dtl
 
 def plot_histogram(df, column, output_path):
     plt.figure(figsize=(10, 6))
@@ -15,10 +15,13 @@ def plot_histogram(df, column, output_path):
 
 
 def main():
-    df = pd.read_csv("./data/processed/RH_procesado.csv")
+    reader = Dtl.DataLoader("./data/processed/RH_procesado.csv")
+
+    df = reader.load_data()
 
     # Asegúrate de que los datos estén correctamente procesados antes de intentar trazarlos
     plot_histogram(df, "Desercion", "./reports/figures/histograma_desercion.png")
+    return df
 
 
 if __name__ == "__main__":
